61ccd54f534cdd9807a8358fd90bbb1f375997b7
Steven Mackey <stevester94@gmail.com>
2021-10-18 18:59:57 -0700

    Add ccn/cida 2

---
+++ chapter3/oshea_snr_1/cnn_2/6/replay.sh
@@ -0,0 +1,121 @@
+#! /bin/sh
+export PYTHONPATH=/usr/local/lib/python3/dist-packages:/usr/local/lib/python3.6/dist-packages
+cat << EOF | ./run.sh -
+{
+  "experiment_name": "OShea SNR CNN",
+  "lr": 0.001,
+  "n_epoch": 200,
+  "batch_size": 128,
+  "patience": 10,
+  "seed": 2017,
+  "device": "cuda",
+  "source_snrs": [
+    -18,
+    -12,
+    -6,
+    0,
+    6,
+    12,
+    18
+  ],
+  "target_snrs": [
+    2,
+    4,
+    8,
+    10,
+    -20,
+    14,
+    16,
+    -16,
+    -14,
+    -10,
+    -8,
+    -4,
+    -2
+  ],
+  "normalize_domain": true,
+  "x_net": [
+    {
+      "class": "Conv1d",
+      "kargs": {
+        "in_channels": 2,
+        "out_channels": 50,
+        "kernel_size": 7,
+        "stride": 1,
+        "padding": 0
+      }
+    },
+    {
+      "class": "ReLU",
+      "kargs": {
+        "inplace": true
+      }
+    },
+    {
+      "class": "Conv1d",
+      "kargs": {
+        "in_channels": 50,
+        "out_channels": 50,
+        "kernel_size": 7,
+        "stride": 2,
+        "padding": 0
+      }
+    },
+    {
+      "class": "ReLU",
+      "kargs": {
+        "inplace": true
+      }
+    },
+    {
+      "class": "Dropout",
+      "kargs": {
+        "p": 0.5
+      }
+    },
+    {
+      "class": "Flatten",
+      "kargs": {}
+    },
+    {
+      "class": "Linear",
+      "kargs": {
+        "in_features": 2900,
+        "out_features": 256
+      }
+    },
+    {
+      "class": "ReLU",
+      "kargs": {
+        "inplace": true
+      }
+    },
+    {
+      "class": "Dropout",
+      "kargs": {
+        "p": 0.5
+      }
+    },
+    {
+      "class": "Linear",
+      "kargs": {
+        "in_features": 256,
+        "out_features": 80
+      }
+    },
+    {
+      "class": "ReLU",
+      "kargs": {
+        "inplace": true
+      }
+    },
+    {
+      "class": "Linear",
+      "kargs": {
+        "in_features": 80,
+        "out_features": 16
+      }
+    }
+  ]
+}
+EOF
